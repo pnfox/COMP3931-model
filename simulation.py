ADDED seed for random generator FIXED bank deposit calculation

simulation.py
@@ -19,6 +19,7 @@ class Simulation:
             beta, # production function parameter
             rCB, # central bank interest rate
             cB, # banks costs
+            seed=None,
             ):
         self.time = time
         self.numberOfFirms = numberOfFirms
@@ -34,6 +35,8 @@ class Simulation:
         self.rCB = rCB
         self.cB = cB
         self.bestFirm = 0
+
+        np.random.seed(seed)
 
         self.firms = agents.Firms(numberOfFirms, self.alpha, self.varpf)
 
@@ -79,7 +82,7 @@ class Simulation:
             # pick up interest of old partner
             currentBank = np.nonzero(self.link_fb[f])
             if not currentBank[0]:
-                oldInterest = np.nan
+                oldInterest = np.inf
             else:
                 oldInterest = self.banks.interestRate[currentBank[0][0]]
 
@@ -87,8 +90,6 @@ class Simulation:
             if (newInterest < oldInterest):
                 #switch
                 self.changeFB[time] = self.changeFB[time] + 1
-
-                # TODO: check for multiple best banks
 
                 # update link
                 self.link_fb[f] = 0
@@ -105,7 +106,7 @@ class Simulation:
         for bank in range(self.numberOfBanks):
             # find who is using bank
             bankCustomers = self.findBankCustomers(bank)
-            self.banks.deposit[bank] = np.sum(self.firms.debt[bankCustomers] - self.banks.networth[bank])
+            self.banks.deposit[bank] = np.sum(self.firms.debt[bankCustomers]) - self.banks.networth[bank]
 
             # bank has gone bankrupt
             if self.banks.deposit[bank] < 0:
@@ -155,6 +156,10 @@ class Simulation:
         defaulted = np.where(self.banks.default == 1)
         self.banks.networth[defaulted] = np.random.uniform(2, size=len(defaulted))
         self.banks.default[defaulted] = 0
+
+        if np.any(np.where(self.firms.default == 1)):
+            print("Error: Defaulted firms not removed")
+            exit()
 
     def updateInterestRates(self):
         self.banks.interestRate = self.gamma * np.float_power(self.banks.networth, -self.gamma)
@@ -285,5 +290,5 @@ class Simulation:
             totalOutput = np.sum(self.firms.output)
             self.firmOutputReport[t] = totalOutput
             self.firmCapitalReport[t] = totalCapital
-#        plt.plot(self.firmCapitalReport)
+#       plt.plot(self.firmCapitalReport)
 #        plt.show()