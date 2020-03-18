a7e6203a1326c6b4e2bf6947080c57d79e66d162
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2020-03-18 17:41:06 +0000

    More sophisticated batchrun

--- batchRun.py
+++ batchRun.py
@@ -2,14 +2,16 @@ import os
 import threading
 import time
 
-def thread_function():
-    os.system("python main.py -t 1000 -f 500 -b 50 -var 0.1")
+def thread_function(batchID, threadID):
+    adj = str(batchID)+str(threadID+2)
+    os.system("python3 main.py -s 10 -t 1000 -f 500 -b 50 -var 0.15 -adj " + \
+            "0." + adj + " --output results/verylowVaradj" + adj + "/")
 
 # Runs many simulation in parallel
-def batchRun():
+def batchRun(n):
     threads = list()
     for i in range(4):
-        thread = threading.Thread(target=thread_function)
+        thread = threading.Thread(target=thread_function, args=(n, i))
         threads.append(thread)
         thread.start()
 
@@ -18,5 +20,5 @@ def batchRun():
 
 if __name__=="__main__":
     
-    for i in range(10):
-        batchRun()
+    for i in range(2):
+        batchRun(i)
